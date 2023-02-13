b4f4079c49487c57fd678b77d44f12bbea8dab0b
melo484b <melodev484b@gmail.com>
2023-02-13 15:24:12 -0500

    Update healthbar logic

--- crustacean-curse-cleanse/ui/HealthBar.gd
+++ crustacean-curse-cleanse/ui/HealthBar.gd
@@ -1,12 +1,15 @@
 extends CenterContainer
 
 
+signal empty
+
+
 onready var bar: TextureProgress = $MarginContainer/PanelContainer/MarginContainer/TextureProgress
 onready var tween: Tween = $Tween
 
 
 func _ready() -> void:
-	take_damage(20)
+	pass
 
 
 func set_value(new_value: int) -> void:
@@ -14,7 +17,12 @@ func set_value(new_value: int) -> void:
 
 
 func take_damage(damage_amount: int) -> void:
+	if bar.value <= 0 or bar.value - damage_amount * 10 <= 0:
+		emit_signal("empty")
+		print("no health")
+# warning-ignore:return_value_discarded
 	tween.interpolate_property(bar, "value",
-		bar.value, bar.value - damage_amount, 1,
+		bar.value, bar.value - damage_amount * 10, 1,
 		Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+# warning-ignore:return_value_discarded
 	tween.start()
