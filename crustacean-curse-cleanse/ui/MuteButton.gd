e29e9767e6d13b913a1a07457f7dde418ebeeefd
melo484b <melodev484b@gmail.com>
2023-02-13 11:53:38 -0500

    Update start menu

---
+++ crustacean-curse-cleanse/ui/MuteButton.gd
@@ -0,0 +1,17 @@
+extends MarginContainer
+
+
+var on: Texture = load("res://art/audio_on.png")
+var off: Texture = load("res://art/audio_off.png")
+var master_sound = AudioServer.get_bus_index("Master")
+
+onready var button: Button = $PanelContainer/MarginContainer/Button
+
+
+func _on_Button_toggled(button_pressed):
+	if button_pressed:
+		button.icon = on
+		AudioServer.set_bus_mute(master_sound, false)
+	else:
+		button.icon = off
+		AudioServer.set_bus_mute(master_sound, true)
