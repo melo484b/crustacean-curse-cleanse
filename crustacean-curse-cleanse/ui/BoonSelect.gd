9e431579611aa47ebb500beafe9f39bc6172d013
melo484b <melodev484b@gmail.com>
2023-02-14 16:09:18 -0500

    Apply ui theme to existing scenes

--- crustacean-curse-cleanse/ui/BoonSelect.gd
+++ crustacean-curse-cleanse/ui/BoonSelect.gd
@@ -2,49 +2,32 @@ extends CanvasLayer
 
 
 var selected: int = 0
-var boon_1_hovered: bool = false
-var boon_2_hovered: bool = false
-var boon_3_hovered: bool = false
 
-func _on_Button_pressed():
+onready var boon_buttons: HBoxContainer = $MarginContainer/PanelContainer/MarginContainer/VBoxContainer/BoonButtons
+
+
+func uncheck_other_buttons(button_name: String) -> void:
+	for button in boon_buttons.get_children():
+		if button.name != button_name:
+			button.pressed = false
+
+
+func _on_Boon1_pressed() -> void:
+	selected = 0
+	uncheck_other_buttons("Boon1")
+
+
+func _on_Boon2_pressed() -> void:
+	selected = 1
+	uncheck_other_buttons("Boon2")
+
+
+func _on_Boon3_pressed() -> void:
+	selected = 2
+	uncheck_other_buttons("Boon3")
+
+
+func _on_Confirm_Button_pressed() -> void:
 	# TODO: Start game
 	PlayerData.set_boon_selection(selected)
-
-
-func _on_Boon_1_mouse_entered():
-	boon_1_hovered = true
-
-
-func _on_Boon_1_mouse_exited():
-	boon_1_hovered = false
-
-
-func _on_Boon_1_gui_input(event):
-	if boon_1_hovered and event.is_action_pressed("attack"):
-		selected = 0
-
-
-func _on_Boon_2_mouse_entered():
-	boon_2_hovered = true
-
-
-func _on_Boon_2_mouse_exited():
-	boon_2_hovered = false
-
-
-func _on_Boon_2_gui_input(event):
-	if boon_2_hovered and event.is_action_pressed("attack"):
-		selected = 1
-
-
-func _on_Boon_3_mouse_entered():
-	boon_3_hovered = true
-
-
-func _on_Boon_3_mouse_exited():
-	boon_3_hovered = false
-
-
-func _on_Boon_3_gui_input(event):
-	if boon_3_hovered and event.is_action_pressed("attack"):
-		selected = 2
+	print(PlayerData.get_boon_selection())
