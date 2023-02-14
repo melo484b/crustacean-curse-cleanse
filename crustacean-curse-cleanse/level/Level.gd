db1ddfd46e0cd0c00dc0cc09af9f3f57f79782e0
melo484b <melodev484b@gmail.com>
2023-02-14 16:59:15 -0500

    Create Level and TestLevel, remove test

---
+++ crustacean-curse-cleanse/level/Level.gd
@@ -0,0 +1,41 @@
+extends Node2D
+
+# Units
+onready var player_unit: PackedScene = preload("res://unit/PlayerUnit.tscn")
+onready var enemy_unit: PackedScene = preload("res://unit/EnemyUnit.tscn")
+# UI objects
+onready var start_menu: PackedScene = preload("res://ui/StartMenu.tscn")
+onready var boon_select: PackedScene = preload("res://ui/BoonSelect.tscn")
+onready var contract_menu: PackedScene = preload("res://ui/ContractMenu.tscn")
+
+onready var ysort: YSort = $YSort
+onready var player_ui: CanvasLayer = $PlayerUI
+
+
+func _ready() -> void:
+	show_start_menu()
+
+
+func show_start_menu() -> void:
+	var start_menu_instance: CanvasLayer = start_menu.instance()
+	add_child(start_menu_instance)
+	start_menu_instance.connect("start_the_game", self, "show_boon_select")
+
+
+func show_boon_select() -> void:
+	var boon_select_instance = boon_select.instance()
+	add_child(boon_select_instance)
+	boon_select_instance.connect("boon_confirmed", self, "show_contract")
+
+
+func show_contract() -> void:
+	var contract_menu_instance = contract_menu.instance()
+	add_child(contract_menu_instance)
+	contract_menu_instance.connect("contract_signed", self, "spawn_player")
+
+
+func spawn_player() -> void:
+	var player = player_unit.instance()
+	player.position = Vector2(512, 300)
+	player.connect("take_damage", player_ui, )
+	ysort.add_child(player)
