325a41130ac1be8922fd408434aef9280bfa065c
melo484b <melodev484b@gmail.com>
2023-02-14 17:12:43 -0500

    Update Level.gd

--- crustacean-curse-cleanse/level/Level.gd
+++ crustacean-curse-cleanse/level/Level.gd
@@ -19,23 +19,26 @@ func _ready() -> void:
 func show_start_menu() -> void:
 	var start_menu_instance: CanvasLayer = start_menu.instance()
 	add_child(start_menu_instance)
+# warning-ignore:return_value_discarded
 	start_menu_instance.connect("start_the_game", self, "show_boon_select")
 
 
 func show_boon_select() -> void:
 	var boon_select_instance = boon_select.instance()
 	add_child(boon_select_instance)
+# warning-ignore:return_value_discarded
 	boon_select_instance.connect("boon_confirmed", self, "show_contract")
 
 
 func show_contract() -> void:
 	var contract_menu_instance = contract_menu.instance()
 	add_child(contract_menu_instance)
+# warning-ignore:return_value_discarded
 	contract_menu_instance.connect("contract_signed", self, "spawn_player")
 
 
 func spawn_player() -> void:
 	var player = player_unit.instance()
 	player.position = Vector2(512, 300)
-	player.connect("take_damage", player_ui, )
+	player.connect("take_damage", player_ui, "reduce_health_bar")
 	ysort.add_child(player)
