cc47688a69914738975f2d795aa4023fca3c979b
melo484b <melodev484b@gmail.com>
2023-02-16 21:08:21 -0500

    Add EnemySpawnManager

---
+++ crustacean-curse-cleanse/manager/EnemySpawnManager.gd
@@ -0,0 +1,17 @@
+extends Node
+
+
+var rng: RandomNumberGenerator = RandomNumberGenerator.new()
+
+onready var enemy: PackedScene = preload("res://unit/EnemyUnit.tscn")
+onready var spawn_layer: YSort = get_parent().get_node("SpawnLayer")
+
+
+func _ready() -> void:
+	spawn_enemy(Vector2(100, 100))
+
+
+func spawn_enemy(spawn_position: Vector2) -> void:
+	var enemy_instance: EnemyUnit = enemy.instance()
+	enemy_instance.position = spawn_position
+	spawn_layer.add_child(enemy_instance)
