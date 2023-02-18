dcbf33983313c95188b7d06be950923950371b27
melo484b <melodev484b@gmail.com>
2023-02-17 20:26:33 -0500

    Implement game timer

---
+++ crustacean-curse-cleanse/ui/GameTimer.gd
@@ -0,0 +1,28 @@
+extends Label
+
+
+signal time_exhausted
+
+
+var game_time: int = 60
+
+
+func update_text() -> void:
+	text = str(game_time)
+
+
+func set_game_time(new_time: int) -> void:
+	game_time = new_time
+
+
+func get_game_time() -> int:
+	return game_time
+
+
+func _on_Timer_timeout() -> void:
+	if game_time > 0:
+		game_time -= 1
+	else:
+		print("time exhausted")
+		emit_signal("time_exhausted")
+	update_text()
--- crustacean-curse-cleanse/ui/PlayerUI.gd
+++ crustacean-curse-cleanse/ui/PlayerUI.gd
@@ -2,6 +2,7 @@ extends CanvasLayer
 
 
 signal player_death
+signal game_time_up
 
 
 onready var health_bar: CenterContainer = $HealthBar
@@ -13,3 +14,7 @@ func reduce_health_bar(damage: float) -> void:
 
 func _on_HealthBar_empty():
 	emit_signal("player_death")
+
+
+func _on_GameTimer_time_exhausted():
+	emit_signal("game_time_up")
