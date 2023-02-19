5388a1ad55dee84ec86c44d24e3fa5746fea3c77
melo484b <melodev484b@gmail.com>
2023-02-19 00:08:01 -0500

    Add light to cannonball explosion

--- crustacean-curse-cleanse/weapon/Cannonball.gd
+++ crustacean-curse-cleanse/weapon/Cannonball.gd
@@ -56,6 +56,7 @@ func increase_detection_size() -> void:
 
 func explode() -> void:
 	$ExplosionSFXmanager.play()
+	$AnimationPlayer.play("explode")
 	exploding = true
 	particles.emitting = true
 	timer.start()
