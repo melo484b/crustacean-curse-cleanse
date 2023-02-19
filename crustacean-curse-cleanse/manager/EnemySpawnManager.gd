0d53ef3a5a43dfb6aac1313f260e2c2ed9f96382
melo484b <melodev484b@gmail.com>
2023-02-19 11:50:43 -0500

    Refactor enemy spawn manager

--- crustacean-curse-cleanse/manager/EnemySpawnManager.gd
+++ crustacean-curse-cleanse/manager/EnemySpawnManager.gd
@@ -1,7 +1,10 @@
 extends Node
 
 
+const MAX_ENEMIES: int = 8
+
 var rng: RandomNumberGenerator = RandomNumberGenerator.new()
+var active_enemies: int = 0
 
 onready var enemy: PackedScene = preload("res://unit/EnemyUnit.tscn")
 onready var spawn_layer: YSort = get_parent().get_node("SpawnLayer")
@@ -9,21 +12,32 @@ onready var wave_timer: Timer = $WaveTimer
 
 
 func _ready() -> void:
+	rng.randomize()
 	wave_timer.start()
 
 
 func spawn_wave() -> void:
-	for child in spawn_layer.get_children():
-		var x_pos: float = rng.randf_range(-10.0, 10.0)
-		if child.is_in_group("FOUNTAIN"):
-			spawn_enemy(child.position + Vector2(x_pos, 0))
-	wave_timer.wait_time = rng.randi_range(2, 9)
+		for child in spawn_layer.get_children():
+			var x_pos: float = rng.randf_range(-10.0, 10.0)
+			var y_pos: float = rng.randf_range(-10.0, 10.0)
+			if child.is_in_group("FOUNTAIN"):
+				if active_enemies < MAX_ENEMIES:
+					spawn_enemy(child.position + Vector2(x_pos, y_pos))
+		wave_timer.wait_time = rng.randi_range(2, 9)
+		wave_timer.start()
 
 
 func spawn_enemy(spawn_position: Vector2) -> void:
 	var enemy_instance: EnemyUnit = enemy.instance()
 	enemy_instance.position = spawn_position
 	spawn_layer.add_child(enemy_instance)
+# warning-ignore:return_value_discarded
+	enemy_instance.connect("enemy_died", self, "decrement_active_enemies")
+	active_enemies += 1
+
+
+func decrement_active_enemies(_position: Vector2) -> void:
+	active_enemies -= 1
 
 
 func _on_WaveTimer_timeout() -> void:
